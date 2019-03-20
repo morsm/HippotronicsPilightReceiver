68c2ba0b99113e8022b6b2b3641052e61426ade7
Maarten ter Mors <maarten@termors.net>
2019-03-20 21:59:02 +0100

    Added stub sender daemon

---
+++ HippotronicsPilightSender/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Termors.Services.HippotronicsPilightSender
+{
+
+    public class ServerConfig
+    {
+        [JsonProperty("ip")]
+        public string IPAddress { get; set; }
+
+        [JsonProperty("port")]
+        public UInt16 Port { get; set; }
+    }
+
+    public class Command
+    {
+        [JsonProperty("unitcode")]
+        public uint Unitcode { get; set; }
+
+        [JsonProperty("command")]
+        public uint Operation { get; set; }
+    }
+
+    public class LampConfig
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("on")]
+        public Command OnCommand { get; set; }
+
+        [JsonProperty("off")]
+        public Command OffCommand { get; set; }
+
+    }
+
+    public class Configuration
+    {
+        [JsonProperty("server")]
+        public ServerConfig Server { get; set; }
+
+        [JsonProperty("lamps")]
+        public LampConfig[] Lamps { get; set; }
+
+    }
+}
---
+++ HippotronicsPilightSender/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace Termors.Services.HippotronicsPilightSender
+{
+    class Program
+    {
+        public static Configuration Configuration;
+
+        static void Main(string[] args)
+        {
+            LoadConfig();
+        }
+
+        private static void LoadConfig()
+        {
+            using (var reader = File.OpenText("config.json"))
+            {
+                var ser = JsonSerializer.Create();
+                var jreader = new JsonTextReader(reader);
+
+                Configuration = ser.Deserialize<Configuration>(jreader);
+            }
+        }
+
+    }
+}
